c80fc7172a46879da89b1266824a7f05d78d584a
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-10-09 16:26:05 -0500

    new project

---
+++ src/main/java/com/valhallagamingstore/valhallags_products/ValhallagsProductsApplication.java
@@ -0,0 +1,13 @@
+package com.valhallagamingstore.valhallags_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValhallagsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValhallagsProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valhallagamingstore/valhallags_products/ValhallagsProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valhallagamingstore.valhallags_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValhallagsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
